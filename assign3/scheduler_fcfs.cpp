986b85262c2b7f93eb5e24e6d6eac7e828d66684
Caden Jamason <me@lutrine.dev>
2024-10-14 15:31:28 -0700

    feat(assign3): :sparkles: add implementation of `SchedulerFCFS`
    add implementation of `SchedulerFCFS` constructor, destructor, member functions, `void init(std::vector<PCB> &process_list)`, `void print_results()`, and `void simulate()`

--- assign3/scheduler_fcfs.cpp
+++ assign3/scheduler_fcfs.cpp
@@ -8,5 +8,79 @@
 
 #include "scheduler_fcfs.h"
 
-// TODO: add implementation of SchedulerFCFS constructor, destructor, and
-// member functions init, print_results, and simulate here
+// Constructor
+SchedulerFCFS::SchedulerFCFS() {
+  current_time = 0;
+  total_waiting_time = 0;
+  total_turnaround_time = 0;
+  avg_waiting_time = 0;
+  avg_turnaround_time = 0;
+}
+
+// Destructor
+SchedulerFCFS::~SchedulerFCFS() {}
+
+// Initialize the scheduler with an array of incoming processes
+void SchedulerFCFS::init(std::vector<PCB> &process_list) {
+  for (PCB &process : process_list) {
+    ready_queue.push_back(process);
+  }
+}
+
+// Print out the statistics, including the turn-around and waiting time of each
+// process and the average turn-around and waiting time
+void SchedulerFCFS::print_results() {
+  for (PCB &process : ready_queue) {
+    std::cout << "T" << process.id << " ";
+    std::cout << "turn-around time = " << process.turnaround_time << ", ";
+    std::cout << "waiting time = " << process.waiting_time << std::endl;
+  }
+  std::cout << "Average turn-around time = " << avg_turnaround_time << ", ";
+  std::cout << "average waiting time = " << avg_waiting_time << std::endl;
+}
+
+// Simulate the scheduling of processes in an FCFS ready-queue
+void SchedulerFCFS::simulate() {
+  // Store the initial size of the ready queue (used to calculate the average
+  // waiting time and turnaround time)
+  int num_of_processes = ready_queue.size();
+  int i = 0;
+
+  while (i < num_of_processes) {
+    // Get the front process from the queue and remove it from the queue
+    PCB &process = ready_queue[i];
+    cout << "Running Process T" << process.id << " for " << process.burst_time
+         << " time units" << endl;
+
+    // If the current time is less than the arrival time, wait for the process
+    // to arrive
+    if (current_time < process.arrival_time) {
+      // Move the current time to the arrival time
+      current_time = process.arrival_time;
+    }
+
+    // Calculate waiting time
+    process.waiting_time = current_time - process.arrival_time;
+
+    // Calculate turnaround time
+    process.turnaround_time = process.waiting_time + process.burst_time;
+
+    // Update statistics
+    total_waiting_time += process.waiting_time;
+    total_turnaround_time += process.turnaround_time;
+
+    // Update current time
+    current_time += process.burst_time;
+
+    // Update the completion time of the process
+    process.completion_time = current_time;
+
+    i++;
+  }
+
+  // After all processes are executed, calculate averages (we cast the total
+  // times as doubles so that integer division does not occur)
+  avg_waiting_time = static_cast<double>(total_waiting_time) / num_of_processes;
+  avg_turnaround_time =
+      static_cast<double>(total_turnaround_time) / num_of_processes;
+}
