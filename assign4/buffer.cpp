a5d653a510e4b48ec9d87a9eb015f801702cdc00
Caden Jamason <me@lutrine.dev>
2024-10-28 08:46:45 -0700

    style: :bulb: fix typos and code format

--- assign4/buffer.cpp
+++ assign4/buffer.cpp
@@ -1,13 +1,11 @@
 /**
-* Assignment 4: Producer Consumer Problem
+ * Assignment 4: Producer Consumer Problem
  * @file buffer.cpp
- * @author ??? (TODO: your name)
+ * @author Caden Jamason, Adrian Reyes
  * @brief Implementation file for the buffer class
  * @version 0.1
  */
 
-// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
 #include "buffer.h"
 
 // TODO: Add your implementation of the buffer class here
