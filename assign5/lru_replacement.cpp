3a8bb933b31184d3984a9ab1fe36314709f5b809
Caden Jamason <me@lutrine.dev>
2024-12-06 20:13:57 -0800

    feat(assign5): :sparkles: implement LRU replacement algorithm

--- assign5/lru_replacement.cpp
+++ assign5/lru_replacement.cpp
@@ -1,41 +1,59 @@
 /**
-* Assignment 5: Page replacement algorithms
+ * Assignment 5: Page replacement algorithms
  * @file lru_replacement.cpp
- * @author ??? (TODO: your name)
+ * @author Caden Jamason and Adrian Reyes
  * @brief A class implementing the LRU page replacement algorithms
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
 
 #include "lru_replacement.h"
 
-// TODO: Add your implementation here
 LRUReplacement::LRUReplacement(int num_pages, int num_frames)
-: Replacement(num_pages, num_frames)
-{
-    // TODO: Complete this constructor
+    : Replacement(num_pages, num_frames) {}
+
+LRUReplacement::~LRUReplacement() {}
+
+void LRUReplacement::touch_page(int page_num) {
+  // Move accessed page to front of list
+  lru_list.erase(page_map[page_num]);
+  lru_list.push_front(page_num);
+  page_map[page_num] = lru_list.begin();
 }
 
-// TODO: Add your implementations for desctructor, touch_page, load_page, replace_page here
-LRUReplacement::~LRUReplacement()
-{
-    // TODO: Add necessary code here
-}
-
-// Accesss a page alreay in physical memory
-void LRUReplacement::touch_page(int page_num)
-{
-    // TODO: Update your data structure LRU replacement
-}
-
-// Access an invalid page, but free frames are available
 void LRUReplacement::load_page(int page_num) {
-    // TODO: Update your data structure LRU replacement and pagetable
+  // Calculate frame number for new page
+  int frame_num = total_frames - free_frames;
+
+  // Update page table with frame number and mark page as valid
+  page_table[page_num].frame_num = frame_num;
+  page_table[page_num].valid = true;
+
+  // Move new page to front of list
+  lru_list.push_front(page_num);
+  page_map[page_num] = lru_list.begin();
 }
 
-// Access an invalid page and no free frames are available
 int LRUReplacement::replace_page(int page_num) {
-    // TODO: Update your data structure LRU replacement and pagetable
-    return 0;
+  // Least recently used page is at the back of the list
+  int victim_page = lru_list.back();
+  lru_list.pop_back();
+
+  // Update page table for victim page
+  page_table[victim_page].valid = false;
+
+  // Use the frame of the victim page for the new page
+  page_table[page_num].frame_num = page_table[victim_page].frame_num;
+
+  // Update page table for new page
+  page_table[page_num].valid = true;
+
+  // Move new page to front of list
+  lru_list.push_front(page_num);
+  page_map[page_num] = lru_list.begin();
+
+  // Remove victim page from map
+  page_map.erase(victim_page);
+
+  // Return victim page number
+  return victim_page;
 }
