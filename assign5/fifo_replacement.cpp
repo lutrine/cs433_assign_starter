2b7e58526f06faa6f243ca3d3797af9335100017
Caden Jamason <me@lutrine.dev>
2024-12-06 20:13:12 -0800

    feat(assign5): :sparkles: implement FIFO replacement algorithm

--- assign5/fifo_replacement.cpp
+++ assign5/fifo_replacement.cpp
@@ -1,34 +1,47 @@
 /**
-* Assignment 5: Page replacement algorithms
+ * Assignment 5: Page replacement algorithms
  * @file fifo_replacement.cpp
- * @author ??? (TODO: your name)
+ * @author Caden Jamason and Adrian Reyes
  * @brief A class implementing the FIFO page replacement algorithms
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
 
 #include "fifo_replacement.h"
 
-// TODO: Add your implementation here
 FIFOReplacement::FIFOReplacement(int num_pages, int num_frames)
-: Replacement(num_pages, num_frames)
-{
-    // TODO: Add additional implementation code
-}
+    : Replacement(num_pages, num_frames) {}
 
-// TODO: Add your implementations for desctructor, load_page, replace_page here
-FIFOReplacement::~FIFOReplacement() {
-    // TODO: Add necessary code here
-}
+FIFOReplacement::~FIFOReplacement() {}
 
-// Access an invalid page, but free frames are available
 void FIFOReplacement::load_page(int page_num) {
-    // TODO: Update your data structure FIFO replacement and pagetable
+  // Calculate frame number for new page
+  int frame_num = total_frames - free_frames;
+
+  // Update page table with frame number and mark page as valid
+  page_table[page_num].frame_num = frame_num;
+  page_table[page_num].valid = true;
+
+  // Add page number to queue
+  queue.push(page_num);
 }
 
-// Access an invalid page and no free frames are available
 int FIFOReplacement::replace_page(int page_num) {
-    // TODO: Update your data structure FIFO replacement and pagetable
-    return 0;
+  // Get oldest page as victim page
+  int victim_page = queue.front();
+  queue.pop();
+
+  // Update the page table for the victim page
+  page_table[victim_page].valid = false;
+
+  // Use the frame of the victim page for the new page
+  page_table[page_num].frame_num = page_table[victim_page].frame_num;
+
+  // Update the page table for the new page
+  page_table[page_num].valid = true;
+
+  // Enqueue the new page
+  queue.push(page_num);
+
+  // Return victim page number
+  return victim_page;
 }
